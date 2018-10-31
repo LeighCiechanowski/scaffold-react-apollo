92ff74e170cc7bdbe66d099df8140627c156bf18
Leigh Ciechanowski <leigh.ciechanowski@sky.uk>
2018-10-31 07:35:29 +0000

    Hooked feed up to GraphQL Query

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { Skeleton, Switch, List, Avatar, Icon } from 'antd';
+import { Skeleton, List, Icon } from 'antd';
 import 'antd/dist/antd.css';
-
-const listData = [];
-for (let i = 0; i < 3; i++) {
-  listData.push({
-    href: 'http://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
 
 const IconText = ({ type, text }) => (
   <span>
@@ -21,38 +10,22 @@ const IconText = ({ type, text }) => (
 );
 
 class Feed extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      loading: true,
-    }
-  }
- 
-
-  onChange = (checked) => {
-    this.setState({ loading: !checked });
-  }
   render() {
-    const { loading } = this.state;
-
     return (
       <div>
-        <Switch checked={!loading} onChange={this.onChange} />
-
         <List
           itemLayout="vertical"
           size="large"
-          dataSource={listData}
+          dataSource={this.props.things}
           renderItem={item => (
             <List.Item
               key={item.title}
-              actions={!loading && [<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+              actions={!this.props.loading && [<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
             >
-              <Skeleton loading={loading} active avatar>
+              <Skeleton loading={this.props.loading} active>
                 <List.Item.Meta
-                  avatar={<Avatar src={item.avatar} />}
                   title={<a href={item.href}>{item.title}</a>}
-                  description={item.description}
+                  description={item.body}
                 />
                 {item.content}
               </Skeleton>
